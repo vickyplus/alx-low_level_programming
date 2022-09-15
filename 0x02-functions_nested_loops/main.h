2271d81c67a67d37cb7918c9fab027c89e7b806c
vickyplus <vickytonnex@gmail.com>
2022-09-14 20:38:22 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+int _putchar(char);
+void print _alphabet(void);
+void print _alphabet_x10(void);
+int _islower(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+int print_isalpha(int c);
+void jack_baver(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
+void num(int r, int n);
+#endif
