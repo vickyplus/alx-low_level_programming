052b9922dca9c0f5fd79862debd3f3cdab84fd3c
vickyplus <vickytonnex@gmail.com>
2022-09-22 09:37:18 -0700

    taskone

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+
+ * _strncat - concatenate two strings but add inputted numbers of bytes
+
+ * 'dest: string to be appended upon
+
+ * @dest: string to be appended upon
+
+ * @src: string to be completed at the end of dest
+
+ * @n:integer parameter to compare index to
+
+ * Return: returns new contanated string
