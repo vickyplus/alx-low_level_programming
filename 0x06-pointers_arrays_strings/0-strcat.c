e58605fddf3c58370ba06977df3ed55cbf06b80f
vickyplus <vickytonnex@gmail.com>
2022-09-22 14:27:41 -0700

    task00

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,24 +1,62 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+
+
+/**
+
+* main - print if the number is postive, zero, or negative
+
+*
+
+* Description: using the main function
+
+* this program prints "Programming is positive, zero, or negative
+
+* Return: 0
+
+*/
+
+int main(void)
+
 {
 
-
-
-int index = 0, dest_len = 0;
-
-	
+int n;
 
 
 
-	while (dest[index++])
+srand(time(0));
 
-		dest_len++;
+n = rand() - RAND_MAX / 2;
 
+  /* your code goes there */
 
+if (n > 0)
 
-	fir (index = 0; src[index]; index++)
+{
 
-	for (index = 0; src[index]; index++)
+	printf("%i is positive\n", n);
 
-		dest[dest_len++] = src[index];
+}
 
+else if (n == 0)
 
+{
+
+	printf("%i is zero\n", n);
+
+}
+
+else if (n < 0)
+
+{
+
+	printf("%i is negative\n", n);
+
+}
+
+return (0);
 
